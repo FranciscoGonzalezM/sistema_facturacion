27dce046039b9dab147e31b9442cde8fa00f1691
FranciscoGonzalezM <gmfrancisco1980@gmail.com>
2025-10-16 23:31:57 -0600

    Eliminada app 'ventas' del proyecto

--- ventas/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'ventas'
-
-urlpatterns = [
-    path('punto-venta/', views.punto_venta, name='punto_venta'),
-    path('reporte-diario/', views.ventas_dia, name='reporte_dia'),
-]

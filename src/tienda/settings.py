ccac476fdc40d2a3eabd51715adb7b7f4ab419a3
FranciscoGonzalezM <gmfrancisco1980@gmail.com>
2025-10-25 20:58:22 -0600

    Corrección de rutas para Render.com - Eliminado create_superuser del manage.py - Actualizado settings_simple.py con rutas src. - Corregidas rutas de templates y archivos estáticos

--- src/tienda/settings.py
+++ src/tienda/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'categorias.apps.CategoriasConfig',
+    'tienda.categorias.apps.CategoriasConfig',
     'productos.apps.ProductosConfig',
     'clientes.apps.ClientesConfig',
     'facturas',
